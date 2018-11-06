7cf6a4c3adb7cd2d4bdd9f7965bb9efc07c5da30
Tobias Richter <tr4711@gmail.com>
2018-11-06 15:34:01 +0100

    Change VPP capacity level to idle EVs

--- simulation.py
+++ simulation.py
@@ -3,17 +3,21 @@ import simpy
 from random import seed, randint
 
 MAX_EV_CAPACITY=16.5 # kWh
-MAX_EV_RANGE=106     # km
-NUM_EVS=20
+MAX_EV_RANGE=20     # km
+NUM_EVS=2
 
 class VPP:
-    def __init__(self, env):
-        self.capacity = simpy.Container(env, init=MAX_EV_CAPACITY*NUM_EVS, capacity=MAX_EV_CAPACITY*NUM_EVS)
+    def __init__(self, env, name):
+        self.capacity = simpy.Container(env, init=0, capacity=MAX_EV_CAPACITY*NUM_EVS)
         self.mon_proc = env.process(self.monitor_capacity(env))
+        self.name = name
 
     def monitor_capacity(self, env):
+        level = 0
         while True:
-            print('Capacity level: %d  at %s' % (self.capacity.level, env.now))
+            if level != self.capacity.level:
+                level = self.capacity.level
+                print('[%s] - %s VPP: Capacity level: %.2f' % (env.now, self.name, self.capacity.level))
             yield env.timeout(1)
 
 class EV:
@@ -26,30 +30,32 @@ class EV:
         while True:
             if self.idle:
                 idle_time = randint(5, 20)
+                yield vpp.capacity.put(self.battery.level)
                 yield env.timeout(idle_time)
-                print('EV %s was idle for %d minutes' % (name, idle_time))
+                print('[%s] - EV %s was idle for %d minutes' % (env.now, name, idle_time))
                 self.idle = False
             else:
                 avg_speed = randint(30, 60) # km/h
                 trip_distance = randint(5, 10) # km
                 trip_time = int(trip_distance / avg_speed * 60) # minutes
                 trip_capacity = MAX_EV_CAPACITY / MAX_EV_RANGE * trip_distance # kWh
-                print('EV %s started driving at %s' % (name, env.now))
+                print('[%s] - EV %s starts driving' % (env.now, name))
+                yield vpp.capacity.get(self.battery.level)
                 yield env.timeout(trip_time + idle_time)
-                yield vpp.capacity.get(trip_capacity)
-                print('EV %s drove %d kilometers in %d minutes and consumed %f kWh' % (name, trip_distance, trip_time, trip_capacity))
+                yield self.battery.get(trip_capacity)
+                print('[%s] - EV %s drove %d kilometers in %d minutes and consumed %.2f kWh' % (env.now, name, trip_distance, trip_time, trip_capacity))
                 self.idle = True
 
 
 def car_generator(env, vpp):
     for i in range(NUM_EVS):
-        print('EV %s joined the VPP at %s' % (i, env.now))
+        print('[%s] - EV %s joined the fleet' % (env.now, i))
         ev = EV(env, vpp, i)
         yield env.timeout(1)
 
 
 env = simpy.Environment()
-vpp = VPP(env)
+vpp = VPP(env, "Balancing")
 # ev = EV(env, vpp, "1")
 car_gen = env.process(car_generator(env, vpp))
 env.run(200)
