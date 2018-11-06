c561bd86c45b74489d67b4946c69605a0647e850
Tobias Richter <tr4711@gmail.com>
2018-11-06 16:44:59 +0100

    Improve logging

--- simulation.py
+++ simulation.py
@@ -10,22 +10,32 @@ NUM_EVS=2
 class VPP:
     def __init__(self, env, name):
         self.capacity = simpy.Container(env, init=0, capacity=MAX_EV_CAPACITY*NUM_EVS)
-        self.mon_proc = env.process(self.monitor_capacity(env))
+        self.env = env
         self.name = name
+        self.mon_proc = env.process(self.monitor_capacity(env))
 
+    def log(self, message):
+        print('[%s] - VPP %s [%.2f/%.2f]'% (self.env.now, self.name, self.capacity.level, self.capacity.capacity), message)
+    
     def monitor_capacity(self, env):
         level = 0
         while True:
             if level != self.capacity.level:
                 level = self.capacity.level
-                print('[%s] - %s VPP: Capacity level: %.2f' % (env.now, self.name, self.capacity.level))
+                self.log('')
             yield env.timeout(1)
 
 class EV:
     def __init__(self, env, vpp, name):
-        self.idle = True
         self.battery = simpy.Container(env, init=MAX_EV_CAPACITY, capacity=MAX_EV_CAPACITY)
         self.drive_proc = env.process(self.drive(env, vpp, name))
+        self.env = env
+        self.name = name
+        
+        self.idle = True
+    
+    def log(self, message):
+        print('[%s] - EV %s [%.2f/%.2f]'% (self.env.now, self.name, self.battery.level, self.battery.capacity), message)
     
     def plugged_in(self):
         return random() <= 0.3
@@ -35,20 +45,20 @@ class EV:
             if self.idle:
                 idle_time = randint(5, 20)
                 yield vpp.capacity.put(self.battery.level)
-                print('[%s] - EV %s is idle' % (env.now, name))
+                self.log('is idle')
 
                 if self.plugged_in():
-                    print('[%s] - EV%s is at charging station' % (env.now, name))
+                    self.log('is at charging station')
                     for i in range(0, idle_time):
                         if self.battery.level < self.battery.capacity - (CHARGING_SPEED / 60):
-                            print('[%s] - Charging EV %s Battery: %.2f' % (env.now, name, self.battery.level))
+                            self.log('is charging')
                             yield self.battery.put(CHARGING_SPEED / 60)
                             yield vpp.capacity.put(CHARGING_SPEED / 60)
                         yield env.timeout(1)
                     
                 else:
                     yield env.timeout(idle_time)
-                print('[%s] - EV %s was idle for %d minutes' % (env.now, name, idle_time))
+                self.log('was idle for %d minutes' % idle_time)
                 self.idle = False
             else:
                 avg_speed = randint(30, 60) # km/h
@@ -57,13 +67,13 @@ class EV:
                 trip_capacity = MAX_EV_CAPACITY / MAX_EV_RANGE * trip_distance # kWh
                 
                 if self.battery.level > trip_capacity:
-                    print('[%s] - EV %s starts driving' % (env.now, name))
+                    self.log('starts driving')
                     yield vpp.capacity.get(self.battery.level)
                     yield env.timeout(trip_time + idle_time)
                     yield self.battery.get(trip_capacity)
-                    print('[%s] - EV %s drove %d kilometers in %d minutes and consumed %.2f kWh' % (env.now, name, trip_distance, trip_time, trip_capacity))
+                    self.log('drove %d kilometers in %d minutes and consumed %.2f kWh'% (trip_distance, trip_time, trip_capacity))
                 else:
-                    print('[%s] - EV %s does not have enough battery for the planned trip' % (env.now, name))
+                    self.log('does not have enough battery for the planned trip')
                     
                 self.idle = True
     
@@ -78,7 +88,7 @@ def car_generator(env, vpp):
 
 
 env = simpy.Environment()
-vpp = VPP(env, "Balancing")
+vpp = VPP(env, 1)
 # ev = EV(env, vpp, "1")
 car_gen = env.process(car_generator(env, vpp))
 env.run(200)
