13591eaa7b5cae8a5b9438b2bf1c85eee61a4386
Tobias Richter <tr4711@gmail.com>
2018-11-10 23:12:10 +0100

    Reorder setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,10 @@ from setuptools import find_packages, setup
 
 setup(
     name='vppsim',
-    packages=find_packages('src'),
     version='0.0.1',
     description='Virtual Power Plant EV Simulation',
-    author='Tobias R.',
-    package_dir={"": "src"},
+    author='Tobias Richter',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
 )
