f5478563c021785195653ff6e60c7f37031845eb
Tobias Richter <tr4711@gmail.com>
2018-11-15 12:29:25 +0100

    Load trip data instead of temporal data

--- src/vppsim/loader.py
+++ src/vppsim/loader.py
@@ -5,7 +5,7 @@ from pathlib import Path
 
 
 PROJECT_DIR = str(Path(__file__).resolve().parents[2])
-CAR2GO_PATH = PROJECT_DIR + '/data/car2go/S-GO2268.csv'
+CAR2GO_PATH = PROJECT_DIR + '/data/car2go/trips.2016.12.01-2017.02.22.csv'
 
 def main():
     print(load())
@@ -13,10 +13,7 @@ def main():
 
 def load():
     df = pd.read_csv(CAR2GO_PATH, sep=';')
-    df['time'] = pd.to_datetime(df['time'])
     return df
-
-
 
 
 if __name__ == '__main__':
