effab760e6a9018a29f42e4658c5409844a8f4b7
Tobias Richter <tr4711@gmail.com>
2018-11-20 17:04:33 +0100

    Add logger to VPP

--- src/vppsim/entities/vpp.py
+++ src/vppsim/entities/vpp.py
@@ -1,3 +1,4 @@
+import logging
 from datetime import datetime
 import simpy
 
@@ -6,13 +7,19 @@ import vppsim
 
 class VPP:
     def __init__(self, env, name, num_evs):
+        self.logger = logging.getLogger('vppsim.vpp')
+
         self.capacity = simpy.Container(env, init=0, capacity=vppsim.MAX_EV_CAPACITY * num_evs)
         self.env = env
         self.name = name
         # self.mon_proc = env.process(self.monitor_capacity(env))
 
     def log(self, message):
-        print('[%s] - VPP-%s(%.2f/%.2f)' % (datetime.fromtimestamp(self.env.now), self.name, self.capacity.level, self.capacity.capacity), message)
+        self.logger.info('[%s] - %s(%.2f/%.2f) %s' % (datetime.fromtimestamp(self.env.now),
+                                                   self.name, self.capacity.level,
+                                                   self.capacity.capacity, message))
+        self.logger.info(self.capacity.put_queue)
+        self.logger.info(self.capacity.get_queue)
 
     def monitor_capacity(self, env):
         while True:
