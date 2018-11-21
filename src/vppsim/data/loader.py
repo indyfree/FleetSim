eee1366c814f424de6e1fc077af0d981d3b023be
Tobias Richter <tr4711@gmail.com>
2018-11-21 21:45:12 +0100

    Process Car2Go data and save to pickle on rebuild

--- src/vppsim/data/loader.py
+++ src/vppsim/data/loader.py
@@ -1,20 +1,34 @@
 #!/usr/bin/env python
 
+import os
 import pandas as pd
 from pathlib import Path
 
+from vppsim import data
+
 
 PROJECT_DIR = str(Path(__file__).resolve().parents[3])
-CAR2GO_PATH = PROJECT_DIR + '/data/car2go/trips.2016.12.01-2017.02.22.csv'
+CAR2GO_PATH = PROJECT_DIR + '/data/raw/car2go/stuttgart.2016.12.01-2017.02.22.csv'
+PROCESSED_DATA_PATH = PROJECT_DIR + '/data/processed'
+PROCESSED_DATA_FILE = PROCESSED_DATA_PATH + '/trips.pkl'
 
 
 def main():
-    print(load())
+    print(load_car2go(rebuild=True))
 
 
-def load_car2go():
-    df = pd.read_csv(CAR2GO_PATH, sep=';')
-    return df
+def load_car2go(rebuild=False):
+    '''Loads processed data into a dataframe, process again if needed'''
+
+
+    if not os.path.exists(PROCESSED_DATA_PATH):
+        os.makedirs(PROCESSED_DATA_PATH)
+
+    if rebuild is True or os.path.isfile(PROCESSED_DATA_FILE) is False:
+        df = pd.read_csv(CAR2GO_PATH)
+        data.process_car2go(df, PROCESSED_DATA_FILE)
+
+    return pd.read_pickle(PROCESSED_DATA_FILE)
 
 
 if __name__ == '__main__':
--- src/vppsim/data/process.py
+++ src/vppsim/data/process.py
@@ -1,6 +1,29 @@
-#!/usr/bin/env python
+import os
+import pandas as pd
 
-def process(df):
 
-    return df
+def process_car2go(df, path):
+    logger = logging.getLogger('vppsim.data')
+    logger.info('Processing Car2Go data..')
 
+    df_trips = pd.DataFrame()
+    for car in df['name'].unique():
+        df_car = df[df['name'] == car]
+        # TODO: Append with .loc
+        df_trips = df_trips.append(calculate_trips(df_car))
+
+    pd.to_pickle(df_trips, path)
+    logger.info('Saved processed data to disk.')
+
+def calculate_trips(df_car):
+    trips = list()
+    prev = df_car.iloc[0,:]
+    for row in df_car.itertuples():
+        if row.address != prev.address :
+            trips.append([prev.name, prev.timestamp, prev.address, prev.coordinates_lat, prev.coordinates_lon, prev.fuel,
+                           row.timestamp, row.address, row.coordinates_lat, row.coordinates_lon, row.fuel, row.charging,
+                           int((row.timestamp - prev.timestamp)/60)])
+        prev = row
+
+    return pd.DataFrame(trips, columns=['EV', 'start_time', 'start_address', 'start_lat', 'start_lon', 'start_soc', 'end_time',
+                                         'end_address', 'end_lat', 'end_lon', 'end_soc', 'end_charging', 'trip_duration'])
