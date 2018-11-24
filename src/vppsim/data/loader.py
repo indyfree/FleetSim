60de5906ae40b588df5593efd2524f142ab4b799
Tobias Richter <tr4711@gmail.com>
2018-11-24 18:42:58 +0100

    Make processed file naming more explicit

--- src/vppsim/data/loader.py
+++ src/vppsim/data/loader.py
@@ -14,7 +14,7 @@ PROJECT_DIR = str(Path(__file__).resolve().parents[3])
 CAR2GO_PATH = PROJECT_DIR + '/data/raw/car2go/'
 PROCESSED_DATA_PATH = PROJECT_DIR + '/data/processed/'
 PROCESSED_DATA_FILE = PROCESSED_DATA_PATH + '/trips.pkl'
-CAR2GO_FILES = ['stuttgart.2016.03.22-2016.11.30.csv', 'stuttgart.2016.12.01-2017.02.22.csv', 'stuttgart.2017.02.23-2017-05-01.csv', 'stuttgart.2017.05.01-2017.10.31f.csv', 'stuttgart.2017.11.01-2018.01.31.csv']
+CAR2GO_FILES = ['stuttgart.2016.03.22-2016.11.30.csv', 'stuttgart.2016.12.01-2017.02.22.csv', 'stuttgart.2017.02.23-2017-05-01.csv', 'stuttgart.2017.05.01-2017.10.31.csv', 'stuttgart.2017.11.01-2018.01.31.csv']
 
 
 def main():
@@ -29,23 +29,26 @@ def load_car2go(rebuild=False):
         os.makedirs(PROCESSED_DATA_PATH)
 
     for f in CAR2GO_FILES:
-        path = PROCESSED_DATA_PATH + 'trips_' + str(CAR2GO_FILES.index(f)) + '.pkl'
+        path = PROCESSED_DATA_PATH + 'trips.' + f.strip('stuttgart.').strip('.csv') + '.pkl'
         if rebuild is True or os.path.isfile(path) is False:
-            df = pd.read_csv(CAR2GO_PATH + f)
             logger.info('Processing %s...' % f)
+            df = pd.read_csv(CAR2GO_PATH + f)
             df = data.process_car2go(df)
             df.to_csv(path + '.csv')
             pd.to_pickle(df, path)
             logger.info('Saved processed %s to disk.' % f)
+        else:
+            logger.info('Processed %s already found on disk' %f)
 
     if rebuild is True or os.path.isfile(PROCESSED_DATA_FILE) is False:
         pkls = []
         logger.info('Concatening all files together...')
-        for i in range(len(CAR2GO_FILES)):
-            path = PROCESSED_DATA_PATH + 'trips_' + str(i) + '.pkl'
+        for f in CAR2GO_FILES:
+            path = PROCESSED_DATA_PATH + 'trips.' + f.strip('stuttgart.').strip('.csv') + '.pkl'
             pkls.append(pd.read_pickle(path))
 
-        df = pd.concat(pkls)
+        df = pd.concat(pkls).sort_values(['start_time']).reset_index()
+        df.to_csv(PROCESSED_DATA_FILE.strip('.pkl') + '.csv')
         pd.to_pickle(df, PROCESSED_DATA_FILE)
         logger.info('Wrote all processed files to %s' % PROCESSED_DATA_FILE)
 
