ff788669ad204478755d3c0ab5f1cc04a2f1b10c
Tobias Richter <tr4711@gmail.com>
2018-11-27 10:16:55 +0100

    Adjust variable naming to new function

--- src/vppsim/data/loader.py
+++ src/vppsim/data/loader.py
@@ -12,16 +12,19 @@ logger = logging.getLogger(__name__)
 PROJECT_DIR = str(Path(__file__).resolve().parents[3])
 CAR2GO_PATH = PROJECT_DIR + '/data/raw/car2go/'
 PROCESSED_DATA_PATH = PROJECT_DIR + '/data/processed/'
-PROCESSED_DATA_FILE = PROCESSED_DATA_PATH + '/trips.pkl'
-CAR2GO_FILES = ['stuttgart.2016.03.22-2016.11.30.csv', 'stuttgart.2016.12.01-2017.02.22.csv', 'stuttgart.2017.02.23-2017-05-01.csv', 'stuttgart.2017.05.01-2017.10.31.csv', 'stuttgart.2017.11.01-2018.01.31.csv']
+PROCESSED_TRIPS_FILE = PROCESSED_DATA_PATH + '/trips.pkl'
+PROCESSED_DEMAND_FILE = PROCESSED_DATA_PATH + '/demand.pkl'
+CAR2GO_FILES = ['stuttgart.2016.03.22-2016.11.30.csv', 'stuttgart.2016.12.01-2017.02.22.csv',
+                'stuttgart.2017.02.23-2017-05-01.csv', 'stuttgart.2017.05.01-2017.10.31.csv',
+                'stuttgart.2017.11.01-2018.01.31.csv']
 
 
 def main():
-    print(load_car2go_trips(rebuild=True))
+    print(load_car2go_demand(rebuild=True))
 
 
 def load_car2go_trips(rebuild=False):
-    '''Loads processed data into a dataframe, process again if needed'''
+    '''Loads processed trip data into a dataframe, process again if needed'''
 
     if not os.path.exists(PROCESSED_DATA_PATH):
         os.makedirs(PROCESSED_DATA_PATH)
@@ -38,7 +41,7 @@ def load_car2go_trips(rebuild=False):
         else:
             logger.info('Processed %s already found on disk' % f)
 
-    if rebuild is True or os.path.isfile(PROCESSED_DATA_FILE) is False:
+    if rebuild is True or os.path.isfile(PROCESSED_TRIPS_FILE) is False:
         pkls = []
         logger.info('Concatening all files together...')
         for f in CAR2GO_FILES:
@@ -46,11 +49,11 @@ def load_car2go_trips(rebuild=False):
             pkls.append(pd.read_pickle(path))
 
         df = pd.concat(pkls).sort_values(['start_time']).reset_index()
-        df.to_csv(PROCESSED_DATA_FILE.strip('.pkl') + '.csv')
-        pd.to_pickle(df, PROCESSED_DATA_FILE)
-        logger.info('Wrote all processed files to %s' % PROCESSED_DATA_FILE)
+        df.to_csv(PROCESSED_TRIPS_FILE.strip('.pkl') + '.csv')
+        pd.to_pickle(df, PROCESSED_TRIPS_FILE)
+        logger.info('Wrote all processed trips files to %s' % PROCESSED_TRIPS_FILE)
 
-    return pd.read_pickle(PROCESSED_DATA_FILE)
+    return pd.read_pickle(PROCESSED_TRIPS_FILE)
 
 def load_car2go_demand(rebuild=False):
     '''Loads processed demand data into a dataframe, process again if needed'''
