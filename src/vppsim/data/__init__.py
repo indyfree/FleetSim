c14dca66fd91485102d81033f85754ba77bd2ea9
Tobias Richter <tr4711@gmail.com>
2018-11-27 10:15:43 +0100

    Make functions available to package

--- src/vppsim/data/__init__.py
+++ src/vppsim/data/__init__.py
@@ -1,3 +1,3 @@
 # flake8: noqa
-from .loader import load_car2go
-from .process import process_car2go
+from .loader import load_car2go_trips, load_car2go_demand
+from .process import process_car2go, calculate_car2go_demand
