cfed67c649cb5a5f59bdfdeff5d3b58a27a3fafc
Tobias Richter <tr4711@gmail.com>
2018-12-13 22:50:42 +0100

    Add functions to package

--- src/vppsim/data/__init__.py
+++ src/vppsim/data/__init__.py
@@ -1,3 +1,4 @@
 # flake8: noqa
+from .balancing import process_tender_results, process_activated_reserve
 from .loader import load_car2go_trips, load_car2go_demand
 from .process import process_car2go, calculate_car2go_demand
