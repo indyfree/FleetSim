005b011149c1b4e5bc3edde11fe9faa90be8c7df
Tobias Richter <tr4711@gmail.com>
2018-12-13 23:55:25 +0100

    Some fixes in notebooks

--- notebooks/03-Electricity-Markets.py
+++ notebooks/03-Electricity-Markets.py
@@ -4,7 +4,7 @@
 # <h1>Table of Contents &lt;br&gt;&lt;/br&gt;<span class="tocSkip"></span></h1>
 # <div class="toc"><ul class="toc-item"><li><span><a href="#Bulk-Data-Merit-Order" data-toc-modified-id="Bulk-Data-Merit-Order-1"><span class="toc-item-num">1&nbsp;&nbsp;</span>Bulk Data Merit Order</a></span><ul class="toc-item"><li><span><a href="#List-of-Tenders-(Ausschreibung)" data-toc-modified-id="List-of-Tenders-(Ausschreibung)-1.1"><span class="toc-item-num">1.1&nbsp;&nbsp;</span>List of Tenders (Ausschreibung)</a></span></li><li><span><a href="#Result-of-Tenders-(Abgegebene-Angebote-/-Allocated-SRL)" data-toc-modified-id="Result-of-Tenders-(Abgegebene-Angebote-/-Allocated-SRL)-1.2"><span class="toc-item-num">1.2&nbsp;&nbsp;</span>Result of Tenders (Abgegebene Angebote / Allocated SRL)</a></span><ul class="toc-item"><li><span><a href="#Supply/Demand-Curve-according-to-bids-and-asks-(According-to-Kahlen)" data-toc-modified-id="Supply/Demand-Curve-according-to-bids-and-asks-(According-to-Kahlen)-1.2.1"><span class="toc-item-num">1.2.1&nbsp;&nbsp;</span>Supply/Demand Curve according to bids and asks (According to Kahlen)</a></span></li></ul></li><li><span><a href="#Activated-Control-Reserve-from-regelleistungen.net" data-toc-modified-id="Activated-Control-Reserve-from-regelleistungen.net-1.3"><span class="toc-item-num">1.3&nbsp;&nbsp;</span>Activated Control Reserve from regelleistungen.net</a></span><ul class="toc-item"><li><span><a href="#Calculate-15-min-Clearing-prices-by-looking-at-activated-reserve" data-toc-modified-id="Calculate-15-min-Clearing-prices-by-looking-at-activated-reserve-1.3.1"><span class="toc-item-num">1.3.1&nbsp;&nbsp;</span>Calculate 15-min Clearing prices by looking at activated reserve</a></span></li><li><span><a href="#Weekly-activated-secondary-control-reserve" data-toc-modified-id="Weekly-activated-secondary-control-reserve-1.3.2"><span class="toc-item-num">1.3.2&nbsp;&nbsp;</span>Weekly activated secondary control reserve</a></span></li></ul></li><li><span><a href="#Double-Check-Numbers" data-toc-modified-id="Double-Check-Numbers-1.4"><span class="toc-item-num">1.4&nbsp;&nbsp;</span>Double Check Numbers</a></span></li></ul></li><li><span><a href="#Calculate-Merit-order-for-each-Week" data-toc-modified-id="Calculate-Merit-order-for-each-Week-2"><span class="toc-item-num">2&nbsp;&nbsp;</span>Calculate Merit order for each Week</a></span></li></ul></div>
 
-# In[2]:
+# In[325]:
 
 
 # Display plots inline
@@ -15,7 +15,7 @@ get_ipython().run_line_magic('load_ext', 'autoreload')
 get_ipython().run_line_magic('autoreload', '2')
 
 
-# In[3]:
+# In[326]:
 
 
 from datetime import datetime
@@ -29,13 +29,14 @@ import seaborn as sns
 
 # ## List of Tenders (Ausschreibung)
 
-# In[4]:
+# In[327]:
 
 
 df_tenders = pd.read_csv("../data/raw/balancing/tenders_2016_2017.csv", sep=';', index_col=False,
                      dayfirst=True, parse_dates=[0, 1, 2, 3,4], infer_datetime_format=True, decimal=',')
 df_tenders = df_tenders[['DATE_FROM', 'DATE_TO', 'GATE_OPEN_TIME', 'GATE_COSURE_TIME', 'PRODUCT', 'TOTAL_DEMAND_[MW]']]
-df_tenders.columns = ['from', 'to', 'gate_opening', 'gate_closure', 'product', 'demand_mw'] df_tenders.head(20)
+df_tenders.columns = ['from', 'to', 'gate_opening', 'gate_closure', 'product', 'demand_mw']
+df_tenders.head(20)
 
 
 # ## Result of Tenders (Abgegebene Angebote / Allocated SRL)
@@ -45,7 +46,7 @@ df_tenders.columns = ['from', 'to', 'gate_opening', 'gate_closure', 'product', '
 # 
 # **Caution: When aggregating take into account not to sum up HT and NT!**
 
-# In[265]:
+# In[328]:
 
 
 df_results = pd.read_csv("../data/raw/balancing/results_2016_2017.csv", sep=';', index_col=False,
@@ -56,17 +57,20 @@ df_results.columns = ['from', 'to', 'product', 'capacity_price_mw','energy_price
 
 df_results = pd.concat([df_results, pd.DataFrame(df_results["product"].str.split('_',1).tolist(),
                                    columns = ['product_type','product_time'])], axis = 1)
+
 df_results.drop('product', axis=1, inplace=True)
 
+df_results
 
-# In[266]:
+
+# In[329]:
 
 
 # Check for negative payment direction in positive control reserve
 df_results.loc[df_results["product_type"] == "POS"]["payment_direction"].unique()
 
 
-# In[267]:
+# In[330]:
 
 
 # Make energy prices negative where provider has to pay for energy
@@ -75,7 +79,7 @@ df_results.drop(["payment_direction"], axis=1, inplace=True)
 df_results = df_results.sort_values(['from', 'product_type', 'product_time', 'energy_price_mwh'])
 
 
-# In[273]:
+# In[331]:
 
 
 print(df_results.size)
@@ -86,7 +90,7 @@ df_results.head(10)
 
 # **Kahlen:** _The data for Stuttgart contains the individual bids and asks with the respective quantities and prices for each 15-minute time interval. From these bids and asks we form the demand and supply curves. The clearing point Q∗ sets the equilibrium, which determines whether the energy market operator settles the asks and bids placed by FleetPower (if the price P from the model is below the market price_ (P.71, Diss)
 
-# In[34]:
+# In[332]:
 
 
 time = "HT"
@@ -129,10 +133,10 @@ for price in df_cp.itertuples():
 
 # ## Activated Control Reserve from regelleistungen.net
 
-# In[236]:
+# In[302]:
 
 
-df_activated = pd.read_csv("../data/raw/balancing/activated_secondary_reserve_2016_2017.csv", sep=';', decimal=',', thousands='.', index_col=False, dayfirst=True, parse_dates=[0], infer_datetime_format=True)
+df_activated = pd.read_csv("../data/raw/balancing/activated_secondary_reserve_2016_2017.csv", sep=';', decimal=',', thousands='.', index_col=false, dayfirst=true, parse_dates=[0], infer_datetime_format=true)
 df_activated.drop(['LETZTE AENDERUNG', 'ERSATZWERT','LETZTE AENDERUNG.1', 'QUAL. NEG', 'QUAL. POS'], axis=1, inplace=True)
 df_activated.columns = ['date', 'from', 'to', 'neg_mw', 'pos_mw']
 hours_minutes_from = df_activated['from'].str.split(":", expand=True)
@@ -145,12 +149,12 @@ df_activated['to'] = pd.to_datetime(df_activated['date'].astype(str) + " " + hou
 df_activated.loc[(df_activated['to'].dt.hour == 0) & (df_activated['to'].dt.minute == 0), 'to'] = df_activated.to + pd.DateOffset(days=1)
 
 df_activated.drop('date', inplace=True, axis=1)
-df_activated.describe()
+df_activated
 
 
 # ### Calculate 15-min Clearing prices by looking at activated reserve
 
-# In[280]:
+# In[334]:
 
 
 # Calculate cumulative sums of every timeslot for every product
@@ -170,10 +174,10 @@ for d in days:
 df_results["cumsum_allocated_mw"] = pd.concat(cumsums)
 
 
-# In[301]:
+# In[351]:
 
 
-get_ipython().run_cell_magic('time', '', 'clearing_prices = list()\n\nfor t in df_activated.iloc[0:1500, ].itertuples():\n    day = pd.to_datetime(t[1])\n    # Find out product time\n    product_time = "HT" if 8 <= day.hour < 20 else "NT"\n    # We are only interested in negative control reserve clearing prices\n    product_type = "NEG"\n    \n    # TODO: Bids are for the next week\n    cp = df_results.loc[(df_results["to"] >= pd.Timestamp(day.date()))\n                        & (df_results["from"] <= pd.Timestamp(day.date()))\n                        & (df_results["product_time"] == time)\n                        & (df_results["product_type"] == product_type)\n                        & (df_results["cumsum_allocated_mw"] >= t.neg_mw)].iloc[0]["energy_price_mwh"]\n    clearing_prices.append(cp)\n\n# print(clearing_prices)\n#df_activated["clearing_prices"] = clearing_prices')
+get_ipython().run_cell_magic('time', '', 'clearing_prices = list()\n\nfor t in df_activated.iloc[0:20, ].itertuples():\n    day = pd.to_datetime(t[1])\n    # Find out product time\n    product_time = "HT" if 8 <= day.hour < 20 else "NT"\n    # We are only interested in negative control reserve clearing prices\n    product_type = "NEG"\n    \n    cp = df_results.loc[(df_results["to"] >= pd.Timestamp(day.date()))\n                        & (df_results["from"] <= pd.Timestamp(day.date()))\n                        & (df_results["product_time"] == product_time)\n                        & (df_results["product_type"] == product_type)\n                        & (df_results["cumsum_allocated_mw"] >= t.neg_mw)\n                       ].iloc[0]["energy_price_mwh"]\n    print(\'Capacity: %s - Price: %s\' % (t.neg_mw, cp))\n#    print(cp.loc[cp.cumsum_allocated_mw >= t.neg_mw].iloc[0])\n    #clearing_prices.append(cp)\n\n# print(clearing_prices)\n#df_activated["clearing_prices"] = clearing_prices')
 
 
 # In[133]:
