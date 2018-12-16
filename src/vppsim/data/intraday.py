8f1473e35476d7a96385e94cb99243f36bd935ed
Tobias Richter <tr4711@gmail.com>
2018-12-16 22:10:12 +0100

    [data/intraday] Calculate clearing prices

---
+++ src/vppsim/data/intraday.py
@@ -0,0 +1,22 @@
+import logging
+import pandas as pd
+
+logger = logging.getLogger(__name__)
+
+
+def calculate_clearing_prices(df):
+    time = df["product_time"].str.split("Q", expand=True)
+    df["delivery_date"] = pd.to_datetime(
+        df["delivery_date"].astype(str)
+        + " "
+        + time[0]
+        + ":"
+        + ((time[1].astype(int) - 1) * 15).astype(str)
+    )
+    df = df.groupby("delivery_date").min()
+    # Transform to EUR/MWh
+    df.loc[:, "unit_price"] = df["unit_price"] / 100
+
+    df = df.loc[:, ["unit_price"]]
+    df.columns = ["unit_price_eur_mwh"]
+    return df
