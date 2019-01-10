dd024ead70b8f7a73e525e3fc1e9e0cde1f94866
Tobias Richter <tr4711@gmail.com>
2019-01-10 23:34:24 +0100

    [src/sim] Add Step consts

--- src/vppsim/simulation.py
+++ src/vppsim/simulation.py
@@ -15,6 +15,11 @@ MAX_EV_RANGE = 160  # km
 TIME_UNIT = 15  # Minutes
 TIME_UNIT_CHARGE = CHARGING_SPEED / (60 / TIME_UNIT)  # kwh
 
+CHARGING_STEP_KWH = CHARGING_SPEED / (60 / 5)  # kwh in 5 minutes charging
+CHARGING_STEP_SOC = (
+    100 * CHARGING_STEP_KWH / MAX_EV_CAPACITY
+)  # SoC in 5 minutes charging
+
 
 def main():
     logger = setup_logger()
