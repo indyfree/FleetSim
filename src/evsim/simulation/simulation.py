30149e6dd6f607112626bdab0383324565989db4
Tobias Richter <tr4711@gmail.com>
2019-01-28 22:11:04 +0100

    [src/sim] Move simulation into dir

--- src/evsim/simulation/simulation.py
+++ src/evsim/simulation/simulation.py
@@ -7,19 +7,6 @@ import os
 from evsim.entities import EV, VPP
 from evsim.data import loader
 
-# PHYSICAL CONSTANTS
-CHARGING_SPEED = 3.6  # 3.6 kWh per hour
-MAX_EV_CAPACITY = 17.6  # kWh
-MAX_EV_RANGE = 160  # km
-TIME_UNIT = 15  # Minutes
-TIME_UNIT_CHARGE = CHARGING_SPEED / (60 / TIME_UNIT)  # kwh
-
-CHARGING_STEP_KWH = CHARGING_SPEED / (60 / 5)  # kwh in 5 minutes charging
-CHARGING_STEP_SOC = (
-    100 * CHARGING_STEP_KWH / MAX_EV_CAPACITY
-)  # SoC in 5 minutes charging
-
-
 def start(name, charging_speed, ev_capacity, max_ev_range):
     logger = setup_logger(name)
 
