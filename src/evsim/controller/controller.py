c1656138cc6b89a4b0e9acff078b1dd4edbc088d
Tobias Richter <tr4711@gmail.com>
2019-02-09 16:29:06 +0100

    [src/control] Add missing whiteline

--- src/evsim/controller/controller.py
+++ src/evsim/controller/controller.py
@@ -2,6 +2,7 @@ import logging
 
 logger = logging.getLogger(__name__)
 
+
 def dispatch_charging(env, vpp):
     logger.info("Dispatch charging: %s" % vpp.evs.keys())
     for _, ev in vpp.evs.items():
