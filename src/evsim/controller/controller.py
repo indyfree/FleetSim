d474166a5ff4dadb29a0127234b2233ff1f8f1a5
Tobias Richter <tr4711@gmail.com>
2019-02-12 20:52:43 +0100

    [src/control] Add logging function

--- src/evsim/controller/controller.py
+++ src/evsim/controller/controller.py
@@ -1,8 +1,16 @@
+from datetime import datetime
 import logging
 
 logger = logging.getLogger(__name__)
 
 
 def charge_fleet(env, fleet, timestep, strategy):
-    logger.info("Charging %d EVs" % len(fleet))
+    log(env, strategy, "Charging %d EVs." % len(fleet))
     strategy(env, fleet, timestep)
+
+
+def log(env, strategy, message):
+    logger.info(
+        "[%s] - %s(%s) %s"
+        % (datetime.fromtimestamp(env.now), "Controller", strategy.__name__, message)
+    )
