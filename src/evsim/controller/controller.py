92eed4a1d57dd514a9c10d8411ca352b60f44658
Tobias Richter <tr4711@gmail.com>
2019-02-14 21:30:55 +0100

    [src/control] Make controller a class

--- src/evsim/controller/controller.py
+++ src/evsim/controller/controller.py
@@ -2,25 +2,30 @@ from operator import attrgetter
 from datetime import datetime
 import logging
 
-logger = logging.getLogger(__name__)
+from evsim.data import loader
 
 
-def charge_fleet(env, fleet, timestep, strategy):
-    """
-        Perform a charging operation on the fleet for a given timestep.
-        Takes a charging strategy and a list of EVs as input.
-    """
+class Controller:
+    def __init__(self, strategy):
+        self.logger = logging.getLogger(__name__)
 
-    strategy(env, fleet, timestep)
+        self.intraday_prices = loader.load_intraday_prices()
+        self.strategy = strategy
 
+    def charge_fleet(self, env, fleet, timestep):
+        """ Perform a charging operation on the fleet for a given timestep.
+            Takes a a list of EVs as input and charges given its strategy.
+        """
 
-def log(env, message):
-    logger.info(
-        "[%s] - %s(%s) %s"
-        % (datetime.fromtimestamp(env.now), "Controller", "regular", message)
-    )
+        self.strategy(env, self, fleet, timestep)
 
+    def log(self, env, message):
+        self.logger.info(
+            "[%s] - %s(%s) %s"
+            % (datetime.fromtimestamp(env.now), "Controller", "regular", message)
+        )
 
-def dispatch(env, fleet, n):
-    s = sorted(fleet, key=attrgetter("battery.level"))
-    return s[:n]
+    def dispatch(self, fleet, criteria, n):
+        """Return n EVs from fleet according to ascending EV criteria"""
+        s = sorted(fleet, key=attrgetter(criteria))
+        return s[:n]
