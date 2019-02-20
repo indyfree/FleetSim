530fa5b51722962f3ab72a9ab5c3e620ed46a163
Tobias Richter <tr4711@gmail.com>
2019-02-20 23:51:53 +0100

    [src/control] Refine intraday strategy

--- src/evsim/controller/strategy.py
+++ src/evsim/controller/strategy.py
@@ -17,24 +17,43 @@ def intraday(env, controller, fleet, timestamp):
     """ Charge available EVs with intraday electricity
         charge others with regulary"""
 
-    # 1. Predict available capacity +30min
-    t = env.now
-    dt = datetime.fromtimestamp(t + (60 * 30))
-    capacity = controller.predict_capacity(controller.fleet_capacity, dt)
+    # NOTE: Assumption: 30min ahead we can procure at price >= clearing price
+    t = datetime.fromtimestamp(env.now + (60 * 30))
 
-    # 2. Bid capacity at intraday market when predicted price cheaper than regular tariff
-    clearing_price = controller.predict_clearing_price(controller.intraday_prices, dt)
-    bid = controller.bid(controller.intraday_prices, dt, clearing_price + 0.5, capacity)
-
-    if bid is not None:
-        controller.log(
-            env,
-            "Bought %2.f kW for %2.f EUR/MWh for 15-min timeslot %s"
-            % (bid[2], bid[1], bid[0]),
+    # Only bid for 15-min timeslots
+    if t.minute % 15 == 0:
+        bid = _submit_bid(
+            controller, controller.fleet_capacity, controller.intraday_prices, t
         )
-    else:
-        controller.log(env, "Nothing bought")
+
+        if bid:
+            controller.log(
+                env,
+                "Bought %.2f kW for %.2f EUR/MWh for 15-min timeslot %s"
+                % (bid[2], bid[1], bid[0]),
+            )
+        else:
+            controller.log(env, "Nothing bought")
 
     # 3. Save in a day ahead consumption plan (t --> (quantity,price))
 
     return True
+
+
+def _submit_bid(controller, df_capacity, df_intraday, timeslot):
+    clearing_price = controller.predict_clearing_price(
+        controller.intraday_prices, timeslot
+    )
+
+    # We don't want to buy more expensive than Industry tariff
+    # TODO: Parametrize and verify Industry Tariff
+    if clearing_price > 250:
+        return None
+
+    # Predict available capacity at t
+    capacity = controller.predict_capacity(df_capacity, timeslot)
+    print(capacity)
+    # Submit bid for predicted capacity at t
+    return controller.bid(
+        controller.intraday_prices, timeslot, clearing_price, capacity
+    )
