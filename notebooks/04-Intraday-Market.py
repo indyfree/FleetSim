7f32373080a1c9559da6003fbce908146b1c259a
Tobias Richter <tr4711@gmail.com>
2019-02-24 22:57:15 +0100

    [nb/intraday] Rerun with new data

--- notebooks/04-Intraday-Market.py
+++ notebooks/04-Intraday-Market.py
@@ -4,7 +4,7 @@
 # <h1>Table of Contents &lt;br&gt;&lt;/br&gt;<span class="tocSkip"></span></h1>
 # <div class="toc"><ul class="toc-item"></ul></div>
 
-# In[3]:
+# In[1]:
 
 
 # Display plots inline
@@ -24,6 +24,8 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 
+from evsim.data import loader
+
 
 # __EPEX Intraday continous market__
 # 
@@ -31,12 +33,16 @@ import seaborn as sns
 # - Hourly contracts for the next day open at 15:00 pm (d-1) for DE, FR, CH & AT 
 # - Hourly contracts for the next day open at 2.00 pm (d-1) for NL & BE 
 # - 30-min contracts for the next day open at 15:30 (d-1) for CH, DE, FR 
-# - 15-min contracts for the next day open at 4.00 pm (d-1) 
+# 
+# 
+# - **15-min contracts for the next day open at 4.00 pm (d-1)**
+# - **LEAD TIME: 5 Minutes, Clearing every 15 Min**
+# - Till when do we trade when we assume average clearing prices?
 
-# In[16]:
+# In[4]:
 
 
-df = pd.read_csv("../data/processed/intraday_prices.csv", parse_dates=[0], infer_datetime_format=True)
+df = loader.load_intraday_prices()
 df["hour"] = df["delivery_date"].dt.hour
 
 # Clip for better visibility of plots
@@ -45,4 +51,12 @@ df["unit_price_eur_mwh"] = df["unit_price_eur_mwh"].clip(-200,200)
 f, ax = plt.subplots(1, 1)
 f.set_size_inches(18.5, 10.5)
 sns.violinplot(x="hour", y="unit_price_eur_mwh", data=df, ax=ax);
+
+
+# In[5]:
+
+
+q = pd.read_pickle("../data/processed/procom_Q.pkl")
+                 
+q[q["exec_time"] > '2018-01-29 18:55:00'].head(100)
 
