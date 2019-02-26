02192a67255d64106618b575bfc08070c2bf9016
Tobias Richter <tr4711@gmail.com>
2019-02-26 14:37:33 +0100

    [src/controller] Restructure strategy

--- src/evsim/controller/strategy.py
+++ src/evsim/controller/strategy.py
@@ -1,4 +1,5 @@
 from datetime import datetime, timedelta
+import pandas as pd
 
 
 def regular(env, controller, fleet, timestep):
@@ -12,44 +13,47 @@ def regular(env, controller, fleet, timestep):
         ev.action = env.process(ev.charge_timestep(timestep))
 
 
+def balancing(env, controller, fleet, timestep):
+    """ Charge predicted available EVs with balancing electricity
+        charge others from regular electricity tariff"""
+
+    # NOTE: Assumption: every day ahead at 16:00,
+    # we can procure at price >= clearing price
+    if datetime.fromtimestamp(env.now).hour == 16:
+        # Purchase for every 15-minute slot of the next day
+        tomorrow = datetime.now().date() + timedelta(days=1)
+
+        # 15-minute intervals for the next day
+        intervals = pd.date_range(
+            start=tomorrow, end=tomorrow + timedelta(days=1), freq="15min"
+        )[:-1]
+
+        for t in intervals:
+            print(t)
+
+    return False
+
+
 def intraday(env, controller, fleet, timestep):
-    """ Charge available EVs with intraday electricity
-        charge others with regulary"""
+    """ Charge predicted available EVs with intraday electricity
+        charge others from regular electricity tariff"""
 
     # NOTE: Assumption: 30min ahead we can procure at price >= clearing price
     t = datetime.fromtimestamp(env.now + (60 * 30))
 
-    # Only bid for 15-min timeslots
+    # 1. Bid for 15-min timeslots
     if t.minute % 15 == 0:
-        bid = _submit_bid(
-            env, controller, controller.fleet_capacity, controller.intraday_prices, t
-        )
+        try:
+            update_consumption_plan(env, controller, controller.intraday, t, 250)
+        except ValueError as e:
+            controller.error(e)
 
-        if not bid:
-            controller.log(env, "Nothing bought.")
-        elif bid[0] in controller.consumption_plan:
-            raise ValueError("%s was already in consumption plan" % bid[0])
-        else:
-            controller.log(
-                env,
-                "Bought %.2f kW for %.2f EUR/MWh for 15-min timeslot %s"
-                % (bid[1], bid[2], bid[0]),
-            )
-
-            # TODO: Better data structure to save 15 min consumption plan
-            # TODO: Save prices
-            # 3. Save in a 30-min ahead consumption plan
-            # Bought capacity will be for 3 * 5-min timeslots
-            for t in [0, 5, 10]:
-                time = bid[0] + timedelta(minutes=t)
-                controller.consumption_plan[time.timestamp()] = bid[1]
-
-    # Intraday charging if in plan
-    intraday_evs = 0
+    # 2. Charge from Intraday if in consumption plan
+    num_intraday_evs = 0
     if env.now in controller.consumption_plan:
         # TODO Pass capacity as param
-        intraday_evs = int(controller.consumption_plan[env.now] // 17.6)
-        evs = controller.dispatch(fleet, criteria="battery.level", n=intraday_evs)
+        num_intraday_evs = int(controller.consumption_plan[env.now] // 17.6)
+        evs = controller.dispatch(fleet, criteria="battery.level", n=num_intraday_evs)
         controller.log(
             env, "Charging %d/%d EVs from intraday market." % (len(evs), len(fleet))
         )
@@ -58,43 +62,45 @@ def intraday(env, controller, fleet, timestep):
 
     # Regular charging all other EVs
     evs = controller.dispatch(
-        fleet, criteria="battery.level", n=len(fleet) - intraday_evs, descending=True
+        fleet,
+        criteria="battery.level",
+        n=len(fleet) - num_intraday_evs,
+        descending=True,
     )
     controller.log(env, "Charging %d/%d EVs regulary." % (len(evs), len(fleet)))
     for ev in evs:
         ev.action = env.process(ev.charge_timestep(timestep))
 
 
-# TODO: Rename method
-def _submit_bid(env, controller, df_capacity, df_intraday, timeslot):
-    try:
-        clearing_price = controller.predict_clearing_price(
-            controller.intraday_prices, timeslot
-        )
-    except ValueError as e:
-        controller.warning(env, "Submitting bid failed %s: %s." % (timeslot, e))
-        return None
-
-    # We don't want to buy more expensive than Industry tariff
-    # TODO: Parametrize and verify Industry Tariff
-    if clearing_price > 250:
+def update_consumption_plan(env, controller, market, timeslot, industry_tariff):
+    predicted_clearing_price = controller.predict_clearing_price(market, timeslot)
+    if predicted_clearing_price > industry_tariff:
         controller.log(env, "The industry tariff is cheaper.")
-        return None
+        return
 
-    # TODO: Predict number of charging EVs instead
-    # Predict available capacity at t
-    capacity = 0
-    try:
-        capacity = controller.predict_capacity(df_capacity, timeslot)
-    except ValueError as e:
-        controller.warning(env, "Submitting bid failed %s: %s." % (timeslot, e))
-        return None
-
-    if capacity == 0:
+    available_capacity = controller.predict_capacity(timeslot)
+    if available_capacity == 0:
         controller.log(env, "No available capacity predicted.")
         return None
 
-    # Submit bid for predicted capacity at t
-    return controller.bid(
-        controller.intraday_prices, timeslot, clearing_price, capacity
-    )
+    # NOTE: Simple strategy to always bid at predicted clearing price
+    bid = market.bid(timeslot, predicted_clearing_price, available_capacity)
+    if bid is None:
+        controller.log(env, "Bid unsuccessful")
+        return
+    elif bid[0] in controller.consumption_plan:
+        raise ValueError("%s was already in consumption plan" % bid[0])
+    else:
+        controller.log(
+            env,
+            "Bought %.2f kW for %.2f EUR/MWh for 15-min timeslot %s"
+            % (bid[1], bid[2], bid[0]),
+        )
+
+        # TODO: Better data structure to save 15 min consumption plan
+        # TODO: Save prices
+        # TODO: Check timestamp() utc??
+        # Bought capacity will be for 3 * 5-min timeslots
+        for t in [0, 5, 10]:
+            time = bid[0] + timedelta(minutes=t)
+            controller.consumption_plan[time.timestamp()] = bid[1]
