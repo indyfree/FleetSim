cdf58d83a6d5f4c31ffcd169269f9c0135a59d37
Tobias Richter <tr4711@gmail.com>
2019-03-12 22:22:37 +0100

    [nb/intraday] Update notebook with lineplot

--- notebooks/04-Intraday-Market.py
+++ notebooks/04-Intraday-Market.py
@@ -15,7 +15,7 @@ get_ipython().run_line_magic('load_ext', 'autoreload')
 get_ipython().run_line_magic('autoreload', '2')
 
 
-# In[1]:
+# In[2]:
 
 
 from datetime import datetime
@@ -39,19 +39,19 @@ from evsim.data import loader
 # - **LEAD TIME: 5 Minutes, Clearing every 15 Min**
 # - Till when do we trade when we assume average clearing prices?
 
-# In[6]:
+# In[5]:
 
 
 df = loader.load_intraday_prices()
 df["hour"] = df["product_time"].dt.hour
 
-# Clip for better visibility of plots
-df["clearing_price_mwh"] = df["clearing_price_mwh"].clip(-200,200)
-
-f, ax = plt.subplots(1, 1)
+f, (ax1, ax2) = plt.subplots(1, 2)
 f.set_size_inches(18.5, 10.5)
-sns.violinplot(x="hour", y="clearing_price_mwh", data=df, ax=ax);
-print(df.dtypes)
+
+sns.lineplot(x="hour", y="clearing_price_mwh", data=df, ax=ax1)
+
+df["clearing_price_mwh"] = df["clearing_price_mwh"].clip(-200,200)
+sns.violinplot(x="hour", y="clearing_price_mwh", data=df, ax=ax2)
 
 
 # In[10]:
