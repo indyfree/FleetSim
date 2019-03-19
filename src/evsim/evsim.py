c28bfab71733f2d7ba098dcccc6bcb51f07da376
Tobias Richter <tr4711@gmail.com>
2019-03-19 21:32:47 +0100

    [src/sim] Set industry tariff to 150

--- src/evsim/evsim.py
+++ src/evsim/evsim.py
@@ -70,7 +70,7 @@ def cli(ctx, debug, name, logs):
 @click.option(
     "-i",
     "--industry-tariff",
-    default=190,
+    default=150,
     help="Flat industry tariff, which the fleet can charge regularly.",
     show_default=True,
 )
