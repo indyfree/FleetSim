22f2d554beb906d301e63fe95f18a08e77ae02c0
Tobias Richter <tr4711@gmail.com>
2019-03-25 23:47:15 +0100

    [chore] Add keras-rl to setup

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ setup(
         "Click>=7.0",
         "dataclasses>=0.6",
         "gym>=0.12",
+        "keras-rl>=0.4.2",
         "numpy>=1.16.1",
         "pandas>=0.23.4",
         "simpy >=3.0.11",
