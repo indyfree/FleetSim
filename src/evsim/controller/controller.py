adb5ea0c39db6f001ee2094dee9f097a08009c2e
Tobias Richter <tr4711@gmail.com>
2019-04-05 10:47:51 +0200

    [src/control] Move account into controller

--- src/evsim/controller/controller.py
+++ src/evsim/controller/controller.py
@@ -225,3 +225,18 @@ class ConsumptionPlan:
 
     def pop(self, timestamp):
         return self.plan.pop(timestamp, 0)
+
+
+class Account:
+    def __init__(self, balance=0):
+        self.balance = balance
+        self.rental_profits = 0
+
+    def add(self, amount):
+        self.balance += amount
+
+    def subtract(self, amount):
+        self.balance -= amount
+
+    def rental(self, price):
+        self.rental_profits += price
--- src/evsim/simulation/__init__.py
+++ src/evsim/simulation/__init__.py
@@ -1,4 +1,3 @@
 # flake8: noqa
-from .account import Account
 from .statistic import Statistic, SimEntry, ResultEntry
 from .simulation import Simulation, SimulationConfig
