74d76a83d0d9a5f74e2e44a46671e8040011d7d7
Tobias Richter <tr4711@gmail.com>
2019-04-07 15:31:53 +0200

    [nb/results] Rework notebook

--- notebooks/07-Results.py
+++ notebooks/07-Results.py
@@ -2,11 +2,11 @@
 # coding: utf-8
 
 # <h1>Table of Contents &lt;br&gt;&lt;/br&gt;<span class="tocSkip"></span></h1>
-# <div class="toc"><ul class="toc-item"><li><ul class="toc-item"><li><span><a href="#Imports-and-Data-loading" data-toc-modified-id="Imports-and-Data-loading-0.1"><span class="toc-item-num">0.1&nbsp;&nbsp;</span>Imports and Data loading</a></span></li></ul></li><li><span><a href="#Intraday" data-toc-modified-id="Intraday-1"><span class="toc-item-num">1&nbsp;&nbsp;</span>Intraday</a></span></li><li><span><a href="#Balancing" data-toc-modified-id="Balancing-2"><span class="toc-item-num">2&nbsp;&nbsp;</span>Balancing</a></span></li><li><span><a href="#Comparison" data-toc-modified-id="Comparison-3"><span class="toc-item-num">3&nbsp;&nbsp;</span>Comparison</a></span><ul class="toc-item"><li><span><a href="#Plot-differences" data-toc-modified-id="Plot-differences-3.1"><span class="toc-item-num">3.1&nbsp;&nbsp;</span>Plot differences</a></span></li></ul></li><li><span><a href="#Demand-Patterns" data-toc-modified-id="Demand-Patterns-4"><span class="toc-item-num">4&nbsp;&nbsp;</span>Demand Patterns</a></span><ul class="toc-item"><li><span><a href="#Yearly-rental-patterns" data-toc-modified-id="Yearly-rental-patterns-4.1"><span class="toc-item-num">4.1&nbsp;&nbsp;</span>Yearly rental patterns</a></span></li><li><span><a href="#Weekly-Pattern-of-connected-EVS" data-toc-modified-id="Weekly-Pattern-of-connected-EVS-4.2"><span class="toc-item-num">4.2&nbsp;&nbsp;</span>Weekly Pattern of connected EVS</a></span></li><li><span><a href="#Daily-Pattern-of-connected-EVS" data-toc-modified-id="Daily-Pattern-of-connected-EVS-4.3"><span class="toc-item-num">4.3&nbsp;&nbsp;</span>Daily Pattern of connected EVS</a></span></li><li><span><a href="#2.4.-Average-Daily-Pattern" data-toc-modified-id="2.4.-Average-Daily-Pattern-4.4"><span class="toc-item-num">4.4&nbsp;&nbsp;</span>2.4. Average Daily Pattern</a></span></li></ul></li></ul></div>
+# <div class="toc"><ul class="toc-item"><li><ul class="toc-item"><li><span><a href="#Imports-and-Data-loading" data-toc-modified-id="Imports-and-Data-loading-0.1"><span class="toc-item-num">0.1&nbsp;&nbsp;</span>Imports and Data loading</a></span></li></ul></li><li><span><a href="#Intraday" data-toc-modified-id="Intraday-1"><span class="toc-item-num">1&nbsp;&nbsp;</span>Intraday</a></span></li><li><span><a href="#Balancing" data-toc-modified-id="Balancing-2"><span class="toc-item-num">2&nbsp;&nbsp;</span>Balancing</a></span></li><li><span><a href="#Integrated" data-toc-modified-id="Integrated-3"><span class="toc-item-num">3&nbsp;&nbsp;</span>Integrated</a></span></li><li><span><a href="#Plots" data-toc-modified-id="Plots-4"><span class="toc-item-num">4&nbsp;&nbsp;</span>Plots</a></span><ul class="toc-item"><li><span><a href="#Style" data-toc-modified-id="Style-4.1"><span class="toc-item-num">4.1&nbsp;&nbsp;</span>Style</a></span></li><li><span><a href="#Fleet-Utilization" data-toc-modified-id="Fleet-Utilization-4.2"><span class="toc-item-num">4.2&nbsp;&nbsp;</span>Fleet Utilization</a></span></li></ul></li></ul></div>
 
 # ## Imports and Data loading
 
-# In[1]:
+# In[2]:
 
 
 # Display plots inline
@@ -17,7 +17,7 @@ get_ipython().run_line_magic('load_ext', 'autoreload')
 get_ipython().run_line_magic('autoreload', '2')
 
 
-# In[13]:
+# In[4]:
 
 
 from datetime import datetime
@@ -26,18 +26,32 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 
-from evsim.data import load_car2go_trips, load_car2go_capacity
+from evsim.data import load
+
+
+# In[11]:
+
+
+pd.set_option('display.float_format', '{:.2f}'.format)
+
+start = "2016-06-01"
+end = "2018-01-01"
+
+def read_results(path):
+    df = pd.read_csv(path)
+    df["timestamp"] = df["timestamp"].apply(lambda x : datetime.fromtimestamp(x))
+    df = df.set_index("timestamp") 
+    df = df[start:end]
+    return df
 
 
 # # Intraday
 
-# In[15]:
+# In[13]:
 
 
-df_i = pd.read_csv("../logs/stats-intraday.csv")
-df_i["timestamp"] = df_i["timestamp"].apply(lambda x : datetime.fromtimestamp(x))
-df_i = df_i.set_index("timestamp")
-df_i.tail()
+df_i = read_results("../results/intraday-benchmark.csv")
+df_i.sum()/1000
 
 
 # # Balancing
@@ -45,95 +59,59 @@ df_i.tail()
 # In[14]:
 
 
-df_b = pd.read_csv("../logs/stats-balancing.csv")
-df_b["timestamp"] = df_b["timestamp"].apply(lambda x : datetime.fromtimestamp(x))
-df_b = df_b.set_index("timestamp")
-df_b.tail()
+df_b = read_results("../results/balancing-benchmark.csv")
+df_b.sum()/1000
 
 
-# # Comparison
+# # Integrated
 
-# ## Plot differences
-
-# In[21]:
+# In[15]:
 
 
-merge = df_i.merge(df_b, left_index=True, right_index=True, how='left')
-ax = merge.plot(y='balance_eur_x', label='Intraday Balance')
-merge.plot(y='balance_eur_y', label='Balancing Balance', ax=ax)
-plt.show()
+df_in = read_results("../results/integrated-benchmark.csv")
+df_in.sum()/1000
 
 
-# In[22]:
+# # Plots
+
+# ## Style
+
+# In[101]:
 
 
-merge = df_i.merge(df_b, left_index=True, right_index=True, how='left')
-ax = merge.plot(y='vpp_charged_kw_x', label='Intraday Charge')
-merge.plot(y='vpp_charged_kw_y', label='Balancing Charge', ax=ax)
-plt.show()
+sns.set(rc={'figure.figsize':(10,6)})
+
+sns.set_context("paper")
+
+sns.set_style("white")
+sns.set_style("ticks")
 
 
-# # Demand Patterns
+palette = sns.cubehelix_palette(5, start=.5, rot=-.75, reverse=True)
+sns.set_palette(palette)
 
-# In[7]:
-
-
-def apply_smoother(df, days):
-    DAY = 12*24
-
-    df['vpp_avg'] = df['ev_vpp'].rolling(
-        window=int(days*DAY)).mean()
-    df['vpp_soc_avg'] = df['vpp_soc'].rolling(
-        window=int(days*DAY)).mean()
-    df['vpp_capacity_kw_avg'] = df['vpp_capacity_kw'].rolling(
-        window=int(days*DAY)).mean()
-
-    return df
+sns.palplot(palette)
 
 
-def plot(df, title, start="2016-12-1", end="2017-04-28"):
-    X = df.loc[start:end][['vpp_avg', 'vpp_soc_avg', 'vpp_capacity_kw_avg']]
-    return X.plot(figsize=(12, 4), title=title)
+# ## Fleet Utilization
+
+# In[102]:
 
 
-# ## Yearly rental patterns
+df_stats = read_results("../results/stats-baseline.csv")
+df_stats["hour"] = df_stats.index.hour
+df_stats.head()
 
-# In[8]:
-
-
-df = apply_smoother(df, days=14)
-plot(df, "Yearly rental patterns");
-
-
-# ## Weekly Pattern of connected EVS
-
-# In[9]:
-
-
-df = apply_smoother(df, days=0.5)
-plot(df, "Weekly rental patterns", start=datetime(
-    2017, 3, 1), end=datetime(2017, 3, 7));
-
-
-# ## Daily Pattern of connected EVS
-
-# In[10]:
-
-
-df = apply_smoother(df, days=1/24)
-plot(df, "Daily rental patterns", start=datetime(
-    2017, 3, 5), end=datetime(2017, 3, 6));
-
-
-# ## 2.4. Average Daily Pattern
-
-# In[11]:
-
-
-df["hour"] = df.index.hour
-
-f, (ax1, ax2) = plt.subplots(1, 2)
-f.set_size_inches(18.5, 10.5)
-sns.violinplot(x="hour", y="fleet_soc", data=df, ax=ax1)
-sns.violinplot(x="hour", y="ev_vpp", data=df, ax=ax2);
+Y = ["available_evs", "charging_evs", "vpp_evs"]
+for y in Y:
+    label = y.split("_")[0].title()
+    label = label.upper() if label == "Vpp" else label
+    ax = sns.lineplot(x="hour", y=y, ci="sd", data=df_stats, label=label)
+    
+    
+sns.despine(offset=10)
+ax.set(xlabel='Hour', ylabel='Number EVs')
+plt.xticks(np.arange(0, 24, 2));
+plt.yticks(np.arange(0, 500, 50));
+plt.savefig("../results/fig/fleet-utilization.png")
 
