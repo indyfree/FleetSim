f388704dd025377dfc9fb2aaa2e478b33b25d167
Tobias Richter <tr4711@gmail.com>
2019-04-09 20:51:06 +0200

    [src/rl] Add DQQN Agent

---
+++ src/evsim/rl/ddqn.py
@@ -0,0 +1,77 @@
+import numpy as np
+
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Flatten
+from keras.optimizers import Adam
+
+from rl.agents.dqn import DQNAgent
+from rl.policy import LinearAnnealedPolicy, EpsGreedyQPolicy
+
+# from rl.policy import BoltzmannQPolicy,
+from rl.memory import SequentialMemory
+
+
+class DDQN:
+    def __init__(self, env):
+        # Get the environment and extract the number of actions.
+        self.env = env
+
+        # self.env.seed(123)
+        np.random.seed(123)
+
+        nb_actions = self.env.action_space.n
+        nb_states = self.env.observation_space.shape
+
+        # Next, we build a very simple model.
+        model = self._build_nn(nb_states, nb_actions)
+
+        memory = SequentialMemory(limit=50000, window_length=1)
+
+        # policy = BoltzmannQPolicy()
+        policy = LinearAnnealedPolicy(
+            EpsGreedyQPolicy(),
+            attr="eps",
+            value_max=1.0,
+            value_min=0.1,
+            value_test=0.1,
+            nb_steps=40000,
+        )
+
+        # Configure and compile our agent:
+        # You can use every built-in Keras optimizer and even the metrics!
+        self.dqn = DQNAgent(
+            model=model,
+            nb_actions=nb_actions,
+            memory=memory,
+            nb_steps_warmup=100,
+            target_model_update=1e-2,
+            policy=policy,
+        )
+        self.dqn.compile(Adam(lr=1e-2), metrics=["mae"])
+
+    def _build_nn(self, nb_states, nb_actions):
+        model = Sequential()
+        model.add(Flatten(input_shape=(1,) + nb_states))
+        model.add(Dense(16))
+        model.add(Activation("relu"))
+        model.add(Dense(16))
+        model.add(Activation("relu"))
+        model.add(Dense(16))
+        model.add(Activation("relu"))
+        model.add(Dense(nb_actions))
+        model.add(Activation("linear"))
+        return model
+
+    def run(self):
+        # Okay, now it's time to learn something! We visualize the training here for show, but this
+        # slows down training quite a lot. You can always safely abort the training prematurely using
+        # Ctrl + C.
+        self.dqn.fit(self.env, nb_steps=100000, visualize=False, verbose=2)
+        # After training is done, we save the final weights.
+        self.dqn.save_weights(
+            "dqn_{}_weights.h5f".format(self.env.spec.id), overwrite=True
+        )
+
+    def test(self):
+        # Finally, evaluate our algorithm for 1 simulation run.
+        self.dqn.test(self.env, nb_episodes=1, visualize=False)
