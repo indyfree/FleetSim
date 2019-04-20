863bd57b2c49dcd6229da832c9990dc5ca2fce97
Tobias Richter <tr4711@gmail.com>
2019-04-20 15:43:55 +0200

    [experiment] Add Accuracy 50-60

---
+++ src/evsim/experiments/DDDQN-50-60.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import gym
+
+from evsim.experiments import setup_logger
+from evsim.rl import DDQN
+
+name = "DDDQN-50-60"
+episodes = 2
+episode_steps = 65334
+
+setup_logger("sim-{}".format(name), write=False)
+env = gym.make("evsim-v0")
+env.imbalance_costs(5000)
+env.prediction_accuracy((50, 60))
+
+dqqn = DDQN(
+    env,
+    name,
+    memory_limit=(episodes * episode_steps),
+    nb_eps=(1.5 * episode_steps),
+    nb_warmup=1000,
+)
+dqqn.run(episodes * episode_steps)
+
+dqqn.test()
